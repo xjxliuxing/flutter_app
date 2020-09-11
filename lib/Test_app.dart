a0d8797d7596c888eb0de797e6688edbeab7d964
xjxliuxing@163.com <19900713op>
2020-09-12 00:38:07 +0800

    第一次提交3

---
+++ lib/Test_app.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class MyApp1 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: "这是我的第一个HelloWord的flutter",
+      home: new Scaffold(
+          appBar: new AppBar(title: new Text("这是我的第一个Flutter的HelloWord")),
+          body: new Center(child: new Text("HelloWord"))),
+    );
+  }
+}
