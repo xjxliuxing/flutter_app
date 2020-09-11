5c8c6113b534cf9228ed8af22165e3f2b9b7c013
xjxliuxing@163.com <19900713op>
2020-09-12 01:14:50 +0800

    测试listView

--- lib/RandomWords.dart
+++ lib/RandomWords.dart
@@ -1,5 +1,6 @@
 import 'package:english_words/english_words.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 /// 添加一个有状态会改变的widget
 class RandomWords extends StatefulWidget {
@@ -10,9 +11,52 @@ class RandomWords extends StatefulWidget {
 }
 
 class RandomWordsState extends State<RandomWords> {
+  final _array = <WordPair>[];
+  final _fontStyle = const TextStyle(fontSize: 18);
+  final  wordPair = WordPair.random();
+
   @override
   Widget build(BuildContext context) {
-    var wordPair = WordPair.random();
-    return new Text(wordPair.asPascalCase);
+//    return new Text(wordPair.asPascalCase);
+    return new Scaffold (
+      appBar: new AppBar(
+        title: new Text('Startup Name Generator'),
+      ),
+      body: _ListView(),
+    );
+  }
+
+  Widget _ListView() {
+    return ListView.builder(
+      padding: const EdgeInsets.all(16.0),
+      // 对于每个建议的单词对都会调用一次itemBuilder，然后将单词对添加到ListTile行中
+      // 在偶数行，该函数会为单词对添加一个ListTile row.
+      // 在奇数行，该函数会添加一个分割线widget，来分隔相邻的词对。
+      // 注意，在小屏幕上，分割线看起来可能比较吃力。
+        itemBuilder: (context, i) {
+          // 在每一列之前，添加一个1像素高的分隔线widget
+          if (i.isOdd) return new Divider();
+
+          // 语法 "i ~/ 2" 表示i除以2，但返回值是整形（向下取整），比如i为：1, 2, 3, 4, 5
+          // 时，结果为0, 1, 1, 2, 2， 这可以计算出ListView中减去分隔线后的实际单词对数量
+          final index = i ~/ 2;
+          // 如果是建议列表中最后一个单词对
+          if (index >= _array.length) {
+            // ...接着再生成10个单词对，然后添加到建议列表
+            _array.addAll(generateWordPairs().take(10));
+          }
+          return _buildRow(_array[index]);
+        }
+
+    );
+  }
+
+  Widget _buildRow(WordPair array) {
+    return new ListTile(
+      title: new Text(
+        wordPair.asPascalCase,
+        style: _fontStyle,
+      ),
+    );
   }
 }
