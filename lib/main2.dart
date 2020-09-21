35d96ce448d615c428f698802b89afa54514e3ef
xjxliuxing@163.com <19900713op>
2020-09-21 23:08:27 +0800

    联系helloWord

---
+++ lib/main2.dart
@@ -0,0 +1,26 @@
+import 'package:english_words/english_words.dart';
+import 'package:flutter/material.dart';
+
+import 'Test_app.dart';
+
+void main() => runApp(new MyApp1());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var wordPair = WordPair.random();
+
+    return new MaterialApp(
+      title: 'Welcome to Flutter',
+      home: new Scaffold(
+        appBar: new AppBar(
+          title: new Text('Welcome to Flutter'),
+        ),
+        body: new Center(
+//          child: new Text('Hello World'),
+          child: new Text(wordPair.asPascalCase),
+        ),
+      ),
+    );
+  }
+}
--- lib/main3.dart
+++ lib/main3.dart
@@ -7,12 +7,12 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        title: "App的HelloWord",
+    return new MaterialApp(
+        title: "我是App的Title",
         home: new Scaffold(
-          appBar: new AppBar(title: new Text("HelloWord")),
+          appBar: new AppBar(title: new Text("我爱学习")),
           body: new Center(
-            child: new Text("我是HelloWord"),
+            child: new Text("学习使我妈快乐！"),
           ),
         ));
   }
